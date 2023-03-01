5e191ba97723a462b8a29a11cb351ab86ed7f1fc
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2023-03-01 00:38:00 -0300

    refactor: productAPI.js update

--- src/services/productAPI.js
+++ src/services/productAPI.js
@@ -1,15 +1,15 @@
 [
     {
         "id": 1,
-        "title": "Pedal Duplo de Bateria Mapex Falcon PF1000TW + Batedores com Peso + Bag",
-        "price": 2700,
-        "description": "Especificações técnicas:\n- Marca: Mapex\n- Modelo: PF1000TW\n- Pedal Duplo de bumbo com corrente dupla\n- Tensão de mola ajustável\n- Batedor 2 faces Feltro/ABS com ajuste de peso\n- Base estabilizadora inteiriça em aço\n- Âncora ajustável\n- Regulagem de ângulo do batedor\n- Batedor 2 faces Feltro/ABS com pesos intercambiáveis de 10g e 20g\n- Sapata mais leve proporciona maior controle com muito menos fadiga\n- Base estabilizadora mais compacta que a dos demais pedais\n- Bag e chaves inclusas",
-        "category": "bateria",
-        "image": ["https://img.olx.com.br/images/96/966305134639672.jpg", "https://img.olx.com.br/images/96/963365256284614.jpg", "https://img.olx.com.br/images/96/967312490476885.jpg", "https://img.olx.com.br/images/96/969381259385641.jpg","https://img.olx.com.br/images/96/963349016667170.jpg","https://img.olx.com.br/images/96/969334857285230.jpg"],
-        "author": "Gabriel",
+        "title": "Violino elétrico",
+        "price": 750,
+        "description": "Violino elétrico\n4/4\nSomente o violino case sem arco\nAtivo\nHarmonizado\nCavalete, pestana e braço",
+        "category": "violino",
+        "image": ["https://img.olx.com.br/images/01/018328494508541.jpg", "https://img.olx.com.br/images/01/011352130078498.jpg", "https://img.olx.com.br/images/01/014393135126601.jpg"],
+        "author": "Paulo Maurício",
         "address": {
-            "city": "Olinda",
-            "state": "PE"
+            "city": "Cariacica",
+            "state": "ES"
         }
     },
     {
@@ -75,6 +75,162 @@
         "address": {
             "city": "Teixeira de Freitas",
             "state": "BA"
+        }
+    },
+    {
+        "id": 7,
+        "title": "Contra baixo Strinberg JBS 50 4 cordas",
+        "price": 1000,
+        "description": "Vendo esse contra baixo Strinberg JBS 50 4 cordas praticamente novo sem nenhum arranhão sem chiado.",
+        "category": "baixo",
+        "image": ["https://img.olx.com.br/images/16/167315374773297.jpg", "https://img.olx.com.br/images/16/169364374336922.jpg", "https://img.olx.com.br/images/16/162339372696087.jpg", "https://img.olx.com.br/images/16/164320734672555.jpg", "https://img.olx.com.br/images/16/165352256534673.jpg"],
+        "author": "Silva",
+        "address": {
+            "city": "Duque de Caxias",
+            "state": "RJ"
+        }
+    },
+    {
+        "id": 8,
+        "title": "Gaita Silver Star",
+        "price": 50,
+        "description": "Comprei essa gaita a 2 anos, mas nunca cheguei a tocar, fiz uma manutenção nela com intenção de começar a tocar, mas não tenho tempo por agora, então estou vendendo.",
+        "category": "gaita",
+        "image": ["https://img.olx.com.br/images/16/165348498785630.jpg", "https://img.olx.com.br/images/16/161349731088659.jpg", "https://img.olx.com.br/images/16/167381010450800.jpg", "https://img.olx.com.br/images/16/163347856403926.jpg"],
+        "author": "Keven Arcanjo",
+        "address": {
+            "city": "Brasília",
+            "state": "DF"
+        }
+    },
+    {
+        "id": 9,
+        "title": "Violão Tagima Califórnia Fishman",
+        "price": 899,
+        "description": "Vendo/Troco\nViolão Tagima Califórnia\nFishman (China)\nRegulado com cordas .012",
+        "category": "violão",
+        "image": ["https://img.olx.com.br/images/02/022341135166868.jpg", "https://img.olx.com.br/images/02/028342737242944.jpg", "https://img.olx.com.br/images/02/023395252897246.jpg"],
+        "author": "Lucas Lopes",
+        "address": {
+            "city": "Sorocaba",
+            "state": "SP"
+        }
+    },
+    {
+        "id": 10,
+        "title": "Pedal Duplo de Bateria Mapex Falcon PF1000TW + Batedores com Peso + Bag",
+        "price": 2700,
+        "description": "Especificações técnicas:\n- Marca: Mapex\n- Modelo: PF1000TW\n- Pedal Duplo de bumbo com corrente dupla\n- Tensão de mola ajustável\n- Batedor 2 faces Feltro/ABS com ajuste de peso\n- Base estabilizadora inteiriça em aço\n- Âncora ajustável\n- Regulagem de ângulo do batedor\n- Batedor 2 faces Feltro/ABS com pesos intercambiáveis de 10g e 20g\n- Sapata mais leve proporciona maior controle com muito menos fadiga\n- Base estabilizadora mais compacta que a dos demais pedais\n- Bag e chaves inclusas",
+        "category": "bateria",
+        "image": ["https://img.olx.com.br/images/96/966305134639672.jpg", "https://img.olx.com.br/images/96/963365256284614.jpg", "https://img.olx.com.br/images/96/967312490476885.jpg", "https://img.olx.com.br/images/96/969381259385641.jpg", "https://img.olx.com.br/images/96/963349016667170.jpg", "https://img.olx.com.br/images/96/969334857285230.jpg"],
+        "author": "Gabriel",
+        "address": {
+            "city": "Olinda",
+            "state": "PE"
+        }
+    },
+    {
+        "id": 11,
+        "title": "Amplificador guitarra fender champion 110",
+        "price": 1400,
+        "description": "Vendo/Troco\nViolão Tagima Califórnia\nFishman (China)\nRegulado com cordas .012",
+        "category": "amplificador",
+        "image": ["https://img.olx.com.br/images/16/160373259398633.jpg", "https://img.olx.com.br/images/16/163368379782936.jpg", "https://img.olx.com.br/images/16/162363735640990.jpg","https://img.olx.com.br/images/16/160313494347128.jpg"],
+        "author": "Cacá",
+        "address": {
+            "city": "Curitiba",
+            "state": "PR"
+        }
+    },
+    {
+        "id": 12,
+        "title": "Acordeon Baile 48 Baixos preta",
+        "price": 1500,
+        "description": "semi nova ideal para iniciantes",
+        "category": "sanfona",
+        "image": ["https://img.olx.com.br/images/15/157351492863618.jpg", "https://img.olx.com.br/images/15/154347134626128.jpg","https://img.olx.com.br/images/15/151385850162678.jpg"],
+        "author": "Jair",
+        "address": {
+            "city": "Curitiba",
+            "state": "PR"
+        }
+    },
+    {
+        "id": 13,
+        "title": "Sax Eagle VG",
+        "price": 1500,
+        "description": "Excelente sax com acabamento lindo. Comprado em 2021. Muito novo e conservado.",
+        "category": "saxofone",
+        "image": ["https://img.olx.com.br/images/09/092329016069375.jpg", "https://img.olx.com.br/images/09/098377376299730.jpg","https://img.olx.com.br/images/09/098368139250824.jpg"],
+        "author": "Sandro Costa",
+        "address": {
+            "city": "Rio Bonito",
+            "state": "RJ"
+        }
+    },
+    {
+        "id": 14,
+        "title": "Tuba EAGLE 3/4",
+        "price": 6700,
+        "description": "Tuba linda , ótimo som , tem alguns detalhes de uso porém tem um som muito bom , ótimo para graves e agudos tonalidade em sib aceito troca e parcelo no cartão.",
+        "category": "tuba",
+        "image": ["https://img.olx.com.br/images/16/161386499625341.jpg", "https://img.olx.com.br/images/16/164397135195298.jpg"],
+        "author": "Matheus",
+        "address": {
+            "city": "São Bento do Sul",
+            "state": "SC"
+        }
+    },
+    {
+        "id": 15,
+        "title": "Trumpet weril sib.",
+        "price": 1500,
+        "description": "Trumpet weril profissional sib, com gatilho automático, dourado com estojo conforme fotos.",
+        "category": "trompete",
+        "image": ["https://img.olx.com.br/images/69/699384127089035.jpg", "https://img.olx.com.br/images/69/696386361520212.jpg","https://img.olx.com.br/images/69/699360601632294.jpg"],
+        "author": "Lucas",
+        "address": {
+            "city": "Rio Bonito",
+            "state": "RJ"
+        }
+    },
+    {
+        "id": 16,
+        "title": "Cavaquinho",
+        "price": 400,
+        "description": "Praticamente novo, nunca foi usado.",
+        "category": "cavaquinho",
+        "image": ["https://img.olx.com.br/images/68/688373725311568.jpg", "https://img.olx.com.br/images/68/687333129229963.jpg","https://img.olx.com.br/images/68/689385602087119.jpg"],
+        "author": "Vitor",
+        "address": {
+            "city": "Araucária",
+            "state": "PR"
+        }
+    },
+    {
+        "id": 17,
+        "title": "Violoncelo Eagle 4/4 CE200",
+        "price": 2600,
+        "description": "Praticamente novo, nunca foi usadoVioloncelo Eagle 4/4, modelo CE 200, seminovo com capa de proteção acompanhado com arco.",
+        "category": "violoncelo",
+        "image": ["https://img.olx.com.br/images/16/163386498017327.jpg", "https://img.olx.com.br/images/16/162397254025927.jpg","https://img.olx.com.br/images/16/169345253537480.jpg","https://img.olx.com.br/images/16/164344612652256.jpg","https://img.olx.com.br/images/16/163380371623290.jpg"],
+        "author": "Rodrigo",
+        "address": {
+            "city": "Salvador",
+            "state": "BA"
+        }
+    },
+    {
+        "id": 18,
+        "title": "Clarinete Eagle CL04",
+        "price": 1150,
+        "description": "Afinação si bemol, acompanha estojo, 2 baquelite, bocal com braçadeira de couro.",
+        "category": "clarinete",
+        "image": ["https://img.olx.com.br/images/02/021377738809123.jpg","https://img.olx.com.br/images/02/026398732559962.jpg","https://img.olx.com.br/images/02/022313497846971.jpg"],
+        "author": "Vinícius",
+        "address": {
+            "city": "Juiz de Fora",
+            "state": "MG"
         }
     }
 ]
