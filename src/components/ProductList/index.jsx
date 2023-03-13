56be3036a24eacc295dd475e2229cb317fc9c213
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-03-13 20:33:10 -0300

    style: Fixing margin top on filter

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -3,7 +3,7 @@ import { ProductCard } from '../ProductCard';
 
 export function ProductList({ searchValue, categorySelected, priceMin, priceMax, stateAbbreviaton, productList }) {
     return (
-        <Row className="col-12 col-sm-9 row-cols-1 row-cols-sm-2 row-cols-lg-3 m-auto">
+        <Row className="col-12 col-sm-9 row-cols-1 row-cols-sm-2 row-cols-lg-3 m-auto mt-0">
             {
                 productList
                     .filter(product =>
